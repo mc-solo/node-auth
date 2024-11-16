f8882c174f2f317932af205481d5819cd01b99e5
Wondwosen <revatoel@gmail.com>
2024-11-16 20:35:30 +0300

    add: routes/auth.js and setup index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const dotenv = require("dotenv");
+const path = require("path");
+const authRoutes = require("./routes/auth");
+
+dotenv.config(); // Load environment variables
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port:", 5000);
+});
