6b95d7ffb85308d7f0dbf46fbf2b5e7966352bf3
Wondwosen <revatoel@gmail.com>
2024-11-19 22:46:03 +0300

    add: hashPassword function to with bcrypt

--- sessions/index.js
+++ sessions/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const app = express();
 const path = require("path");
+const bcrypt = require("bcryptjs");
 const port = process.env.port || 5000;
 
 app.use(
@@ -13,13 +14,37 @@ app.use(
   })
 );
 
-// dummy user data base
-const users = [
-  { id: 1, username: "Wondwosen", password: "password123", role: "admin" },
-  { id: 2, username: "john", pasword: "1234", role: "user" },
-];
+async function hashPassword(password) {
+  try {
+    await bcrypt.hash(password, 10, (err, hash) => {
+      if (err) {
+        throw err;
+      }
+      return hash;
+    });
+  } catch {
+    console.error("Error hashing password!");
+  }
+}
 
-// middleware to parse json data
+const users = [
+  {
+    id: 1,
+    username: "Wondwosen",
+    password: hashPassword("passwor123"),
+    role: "admin",
+  },
+  {
+    id: 2,
+    username: "john",
+    pasword: hashPassword("123password"),
+    role: "user",
+  },
+];
+console.log("password for user1", users.at(0).password);
+console.log();
+console.log("password for user2", users.at(1).password);
+
 app.use(express.json());
 
 app.use(
@@ -46,6 +71,16 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "sessions", "routs", "login.html"));
 });
-app.listen(port, () => {
-  console.log("app is running on port number:", port);
+
+app.post("login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find((u) => u.username === username);
+  if (!user) {
+    return res.status(400).send("Invalid username or password");
+  }
+
+  // check for password
+
+  const isMatch = await bc;
 });
